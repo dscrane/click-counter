5c50abdc3eb3359fb24e4a80ec113ca53a75f1d2
dscrane <daegancrane@gmail.com>
2021-02-03 07:32:12 -0500

    finished section

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme'
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug())
 });
